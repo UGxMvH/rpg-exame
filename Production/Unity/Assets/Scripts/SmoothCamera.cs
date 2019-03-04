fc873a2969f2d37f84bf78513646f6ab088cc0e0
Menno van Hout <menno.vanhout@universal-games.nl>
2019-03-04 13:16:15 +0100

    Removed: - old tiles
    Added:
    - Unity 2D pixel perfect package
    
    Changed:
    - Camera system tracking player now and using pixel perfect

---
+++ Production/Unity/Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    public Transform target;
+    [Range(0.1f, 1f)]
+    public float smoothness;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get target location
+        Vector3 targetPos = target.position;
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothness);
+    }
+}
