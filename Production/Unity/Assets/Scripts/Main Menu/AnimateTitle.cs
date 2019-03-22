a9b46ddd9af19fb4fbc200a101744a0698de4518
Menno van Hout <menno.vanhout@universal-games.nl>
2019-03-22 10:46:42 +0100

    Added: - Main Menu - Title animation - Button animations - Quit game option - Go to overworld option

---
+++ Production/Unity/Assets/Scripts/Main Menu/AnimateTitle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(RectTransform))]
+public class AnimateTitle : MonoBehaviour
+{
+    RectTransform rect;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+
+        StartCoroutine(Animate());
+    }
+
+    private IEnumerator Animate()
+    {
+        rect.DOShakeScale(100, .1f, 2);
+
+        yield return new WaitForEndOfFrame();
+    }
+}
---
+++ Production/Unity/Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class MainMenu : MonoBehaviour
+{
+    [Header("Button")]
+    public Vector3 scaleHover;
+
+    [Header("Sounds")]
+    public AudioClip hoverButton;
+
+    private new AudioSource audio;
+    private RectTransform rect;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        rect = GetComponent<RectTransform>();
+    }
+
+    public void GoToOverworld()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OnHoverEnter(RectTransform rect)
+    {
+        audio.PlayOneShot(hoverButton);
+        rect.DOBlendableScaleBy(scaleHover, .5f);
+    }
+
+    public void OnHoverExit(RectTransform rect)
+    {
+        rect.DOBlendableScaleBy(-scaleHover, .5f);
+    }
+}
